48b5980c9a9b55ca8e8ccbce1c1d4b905f17739e
WhiteTiger “sgama la rete” <angelwhite@users.sf.net>
2018-10-26 16:34:36 +0200

    …

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ echo "ciao";
 
 $PAGE = <<<EOF
 <a href="demo.php">DEMO</a>
-<a href="https://test-universallink.heroku.com/demo.php">DEMO 2</a>
+<a href="https://test-universallink.herokuapp.com/demo.php">DEMO 2</a>
 EOF;
 
 echo $PAGE;
