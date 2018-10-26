6541789321dcac4aff1d608cb94cff174835745d
WhiteTiger “sgama la rete” <angelwhite@users.sf.net>
2018-10-26 17:21:35 +0200

    ...

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ echo "ciao";
 $PAGE = <<<EOF
 <a href="demo.php">DEMO</a>
 <a href="https://test-universallink.herokuapp.com/demo.php">DEMO 2</a>
+<a href="https://www.timmusic.it/accountBloccatiAlice?m=1">DEMO 3</a>
+
 EOF;
 
 echo $PAGE;
